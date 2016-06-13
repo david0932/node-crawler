af562664526d8b01881fef823b1e0b07d9785a73
david0932 <david0932@yahoo.com.tw>
2016-06-13 23:59:27 +0800

    update -3

--- node-crawler-3.js
+++ node-crawler-3.js
@@ -5,10 +5,10 @@ var cheerio = require("cheerio");
 // Fetch a URL and parse all it's tables into JSON, using promises 
 var tabletojson = require('tabletojson');
 var url = 'http://web.pcc.gov.tw/pishtml/todaytender.html';
-tabletojson.convertUrl(url)
-.then(function(tablesAsJson) {
+tabletojson.convertUrl(url).then(function(tablesAsJson) {
   var table = tablesAsJson[3];
   //var td2 = tablesAsJson[4];
   //console.log(td1,',',td2,'\n');
   console.log(table);
+  fs.writeFileSync("table.json", JSON.stringify(table));
 });
